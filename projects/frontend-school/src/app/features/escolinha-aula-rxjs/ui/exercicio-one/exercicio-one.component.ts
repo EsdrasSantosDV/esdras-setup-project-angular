409a16b52277258d586ace95ce82d02bc542b474
Diogogc1 <diogogomescastro@outlook.com>
2025-04-25 13:25:41 -0300

    feat: diogo-gomes Exercise 1 rxjs

--- projects/frontend-school/src/app/features/escolinha-aula-rxjs/ui/exercicio-one/exercicio-one.component.ts
+++ projects/frontend-school/src/app/features/escolinha-aula-rxjs/ui/exercicio-one/exercicio-one.component.ts
@@ -1,11 +1,47 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Observable, Subject, Subscription, switchMap, take } from 'rxjs';
 
 @Component({
   selector: 'esdras-khan-exercicio-one',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './exercicio-one.component.html',
   styleUrl: './exercicio-one.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ExercicioOneComponent {}
+
+/*
+  Explicação: Criei um observable chamado secondCounter e um subject chamado reset
+  
+  Na Inicialização do componente, o secondCounter recebe um observable que vem do reset com um swtichMap
+  que tem um interval dentro dele, isso significa que quando o reset emitir um valor, o switchMap vai sobrepor
+  o interval por um novo, fazendo com que secondCounter seja sobreposto com um novo observable.
+
+  O metodo reset emite um valor para reset, ativando o switchMap
+
+  O ngOnDestroy é utilizado para cancelar a subscrição no secondCounter, evitando vazamentos de memória.
+*/
+export class ExercicioOneComponent implements OnInit, OnDestroy {
+  secondCounter$!: Observable<number>;
+  reset$ = new Subject<void>();
+  private subscription!: Subscription;
+
+  ngOnInit(): void {
+    this.secondCounter$ = this.reset$.pipe(switchMap(() => interval(1000).pipe(take(11))));
+    this.subscription = this.secondCounter$.subscribe();
+    setTimeout(() => {
+      this.reset$.next();
+    });
+  }
+
+  reset(): void {
+    this.reset$.next();
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+}
--- projects/frontend-school/src/app/features/escolinha-aula-rxjs/ui/exercicio-two/exercicio-two.component.ts
+++ projects/frontend-school/src/app/features/escolinha-aula-rxjs/ui/exercicio-two/exercicio-two.component.ts
@@ -1,11 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'esdras-khan-exercicio-two',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './exercicio-two.component.html',
   styleUrl: './exercicio-two.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ExercicioTwoComponent {}
+export class ExercicioTwoComponent {
+  textControl = new FormControl('');
+}
