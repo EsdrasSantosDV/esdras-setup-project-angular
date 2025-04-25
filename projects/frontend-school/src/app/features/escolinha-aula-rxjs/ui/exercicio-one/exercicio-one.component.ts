1dcaff0158e02a15ff1dcbb27d17a25cb1985725
Artur C. Miranda <artcamir12@gmail.com>
2025-04-25 09:21:26 -0300

    ex 1

--- projects/frontend-school/src/app/features/escolinha-aula-rxjs/ui/exercicio-one/exercicio-one.component.ts
+++ projects/frontend-school/src/app/features/escolinha-aula-rxjs/ui/exercicio-one/exercicio-one.component.ts
@@ -1,11 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'esdras-khan-exercicio-one',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './exercicio-one.component.html',
   styleUrl: './exercicio-one.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ExercicioOneComponent {}
+export class ExercicioOneComponent {
+  fonte$ = interval(1000);
+}
