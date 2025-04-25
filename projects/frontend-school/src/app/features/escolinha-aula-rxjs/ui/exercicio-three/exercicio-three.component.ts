204a9481b01432694f3566c6ba70fbebf9c95263
Diogogc1 <diogogomescastro@outlook.com>
2025-04-25 13:25:41 -0300

    feat: diogo-gomes Exercises 1 and 2

--- projects/frontend-school/src/app/features/escolinha-aula-rxjs/ui/exercicio-three/exercicio-three.component.ts
+++ projects/frontend-school/src/app/features/escolinha-aula-rxjs/ui/exercicio-three/exercicio-three.component.ts
@@ -1,11 +1,36 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, OnInit, signal } from '@angular/core';
+import { interval, map, range, take, tap } from 'rxjs';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { FormBuilder, FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'esdras-khan-exercicio-three',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, JsonPipe, FormsModule, ReactiveFormsModule],
   templateUrl: './exercicio-three.component.html',
   styleUrl: './exercicio-three.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ExercicioThreeComponent {}
+export class ExercicioThreeComponent implements OnInit {
+  fb = inject(FormBuilder);
+
+  string: string = 'Maria';
+
+  interval$ = interval(1000).pipe(take(10));
+  printedValue$ = this.interval$.pipe(
+    map((value) => value * 2),
+    tap((value) => {
+      this.string += String(value);
+      this.reactiveForm.controls['name'].setValue(this.string);
+    }),
+  );
+
+  reactiveForm = this.fb.group({
+    name: [''],
+  });
+
+  ngOnInit(): void {
+    this.printedValue$.subscribe();
+  }
+}
