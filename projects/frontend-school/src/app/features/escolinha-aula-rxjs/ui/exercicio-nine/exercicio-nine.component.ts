f2e241b4261d43ea6611777a6a93ce69c03d6105
Diogo Gomes <diogogomescastro@outlook.com>
2025-05-04 22:32:24 -0300

    feat: diogo-gomes Exercise 09

--- projects/frontend-school/src/app/features/escolinha-aula-rxjs/ui/exercicio-nine/exercicio-nine.component.ts
+++ projects/frontend-school/src/app/features/escolinha-aula-rxjs/ui/exercicio-nine/exercicio-nine.component.ts
@@ -1,11 +1,25 @@
+import { AsyncPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { filter, first, range } from 'rxjs';
 
 @Component({
   selector: 'esdras-khan-exercicio-nine',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './exercicio-nine.component.html',
   styleUrl: './exercicio-nine.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ExercicioNineComponent {}
+
+/* 
+  Explicação: 
+  - Usei um range de 1 a 10
+  - Usei o operador filter para filtrar os valores maiores que 5
+  - Usei o operador first para pegar o primeiro valor que passar pelo filtro
+*/
+export class ExercicioNineComponent {
+  fisrtNumber$ = range(1, 10).pipe(
+    filter((value) => value > 5),
+    first(),
+  );
+}
