6be9f7c0a2affc8ae34c189f15af12fa4c1eaa55
Diogo Gomes <diogogomescastro@outlook.com>
2025-05-09 03:07:46 -0300

    feat: diogo-gomes Exercise 19

---
+++ projects/frontend-school/src/app/features/escolinha-aula-rxjs/ui/exercise-nineteen/exercise-nineteen.component.ts
@@ -0,0 +1,36 @@
+import { AsyncPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { delay, exhaustMap, Observable, of, Subject, tap } from 'rxjs';
+
+@Component({
+  selector: 'esdras-khan-exercise-nineteen',
+  standalone: true,
+  imports: [AsyncPipe],
+  templateUrl: './exercise-nineteen.component.html',
+  styleUrl: './exercise-nineteen.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+
+/*
+  Explicação:
+  - O operador `exhaustMap` é usado para ignorar novas emissões enquanto uma emissão anterior ainda está em andamento.
+  Isso é útil em cenários onde você não quer que múltiplos cliques ou eventos acionem múltiplas requisições simultâneas.
+*/
+export class ExerciseNineteenComponent {
+  private loginClick$ = new Subject<void>();
+  loginResult$: Observable<string>;
+
+  constructor() {
+    this.loginResult$ = this.loginClick$.pipe(
+      exhaustMap(() => this.simulateLogin().pipe(tap((response) => console.log(response)))),
+    );
+  }
+
+  onClick() {
+    this.loginClick$.next();
+  }
+
+  simulateLogin() {
+    return of('Login bem-sucedido!').pipe(delay(1000));
+  }
+}
