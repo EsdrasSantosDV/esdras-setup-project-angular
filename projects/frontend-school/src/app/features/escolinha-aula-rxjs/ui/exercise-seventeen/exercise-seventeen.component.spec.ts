35ad6d014b142a1bed188f8f9e7e254defe6ba56
Diogo Gomes <diogogomescastro@outlook.com>
2025-05-09 03:05:36 -0300

    feat: diogo-gomes Exercise 17

---
+++ projects/frontend-school/src/app/features/escolinha-aula-rxjs/ui/exercise-seventeen/exercise-seventeen.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ExerciseSeventeenComponent } from './exercise-seventeen.component';
+
+describe('ExerciseSeventeenComponent', () => {
+  let component: ExerciseSeventeenComponent;
+  let fixture: ComponentFixture<ExerciseSeventeenComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ExerciseSeventeenComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ExerciseSeventeenComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ projects/frontend-school/src/app/features/escolinha-aula-rxjs/ui/exercise-seventeen/exercise-seventeen.component.ts
@@ -0,0 +1,40 @@
+import { AsyncPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { BehaviorSubject, concatMap, delay, Observable, of, tap } from 'rxjs';
+
+@Component({
+  selector: 'esdras-khan-exercise-seventeen',
+  standalone: true,
+  imports: [AsyncPipe],
+  templateUrl: './exercise-seventeen.component.html',
+  styleUrl: './exercise-seventeen.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+
+/*
+  Explicação:
+  - O componente usa o BehaviorSubject para armazenar os cliques.
+  - O clickSubject é um BehaviorSubject que armazena o número de cliques.
+  - O concatMap é usado para encadear as requisições, fazendo com que
+  uma nova requisição só começo depois da anterior acabar.
+  - O método onClick() incrementa o número de cliques e aciona a requisição.
+*/
+export class ExerciseSeventeenComponent implements OnInit {
+  private clickSubject = new BehaviorSubject<number>(0);
+  clickResult$!: Observable<string>;
+
+  ngOnInit() {
+    this.clickResult$ = this.clickSubject.pipe(
+      concatMap((clickNumber) =>
+        of(`Requisição ${clickNumber} concluída`).pipe(
+          delay(1000),
+          tap((response) => console.log(response)),
+        ),
+      ),
+    );
+  }
+
+  onClick() {
+    this.clickSubject.next(this.clickSubject.value + 1);
+  }
+}
