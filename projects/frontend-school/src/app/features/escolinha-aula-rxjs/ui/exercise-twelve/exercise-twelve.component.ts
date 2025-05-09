2e99bc6084c2a45c5cb1b407020a5ad26fd29e13
brenom137 <breno.freitas@graodireto.com.br>
2025-05-09 08:50:00 -0300

    feat: exercise twelve fix

--- projects/frontend-school/src/app/features/escolinha-aula-rxjs/ui/exercise-twelve/exercise-twelve.component.ts
+++ projects/frontend-school/src/app/features/escolinha-aula-rxjs/ui/exercise-twelve/exercise-twelve.component.ts
@@ -1,11 +1,26 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'esdras-khan-exercise-twelve',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './exercise-twelve.component.html',
   styleUrl: './exercise-twelve.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ExerciseTwelveComponent {}
+export class ExerciseTwelveComponent implements AfterViewInit {
+  @ViewChild('btnSearch') btnSearch!: ElementRef;
+
+  formGroup = new FormGroup({
+    search: new FormControl(''),
+  });
+
+  ngAfterViewInit() {
+    fromEvent(this.btnSearch.nativeElement, 'click').subscribe(() => {
+      const inputValue = this.formGroup.get('search')?.value;
+      console.log('Valor do campo de texto:', inputValue);
+    });
+  }
+}
